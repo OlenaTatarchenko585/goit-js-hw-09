c9741f5e2a49b3da869893ebe1ef4505ec62c239
Olena Tatarchenko <olenatatarch585@gmail.com>
2026-03-09 20:54:01 +0200

    fix:  removed trim from input

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,7 +3,6 @@ let formData = { email: "", message: "" };
 const form = document.querySelector(".feedback-form");
 const STORAGE_KEY = "feedback-form-state";
 
-
 const savedData = localStorage.getItem(STORAGE_KEY);
 if (savedData) {
   formData = JSON.parse(savedData);
@@ -11,24 +10,26 @@ if (savedData) {
   form.elements.message.value = formData.message;
 }
 
-
 form.addEventListener("input", (event) => {
   const { name, value } = event.target;
-  formData[name] = value.trim();
+  formData[name] = value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 });
-
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  if (formData.email === "" || formData.message === "") {
+  const trimmedEmail = formData.email.trim();
+  const trimmedMessage = formData.message.trim();
+
+   if (trimmedEmail === "" || trimmedMessage === "") {
     alert("Fill please all fields");
     return;
   }
 
-  console.log(formData);
-  localStorage.removeItem(STORAGE_KEY);
+    console.log({ email: trimmedEmail, message: trimmedMessage });
+
+   localStorage.removeItem(STORAGE_KEY);
   formData = { email: "", message: "" };
   form.reset();
 });
